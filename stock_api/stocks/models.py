59e92b44106d96dc00aa1bfa50eac2a12fee105c
pythonredhat <pythonredhat1987@gmail.com>
2019-08-18 15:16:30 -0400

    Add stock model

--- stock_api/stocks/models.py
+++ stock_api/stocks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+    """
+    Stock Model
+    Defines the attributes of a stock
+    """
+
+    name = models.CharField(max_length=255)
+    ticker = models.CharField(max_length=255)
+    market_cap = models.IntegerField()
+    eps = models.FloatField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_stock(self):
+        return self.name + ' is worth ' + self.market_cap
+    
+    def __repr__(self):
+        return self.name + ' is added.'
