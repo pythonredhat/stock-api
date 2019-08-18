bd4fa932095fdf2f729199ad77624348ac2aa641
pythonredhat <pythonredhat1987@gmail.com>
2019-08-18 15:36:22 -0400

    Fix indent

--- stock_api/stocks/tests/test_models.py
+++ stock_api/stocks/tests/test_models.py
@@ -10,12 +10,12 @@ class StockTest(TestCase):
         Stock.objects.create(
             name='Mastercard', ticker='MA', market_cap=278355000000, eps=6.48)
 
-     def test_stock_worth(self):
-         stock_visa = Stock.objects.get(name='Visa')
-         stock_mastercard = Stock.objects.get(name='Mastercard')
-         self.assertEqual(
-             stock_visa.get_stock_value(), "Visa is worth 399306000000")
-         self.assertEqual(
-             stock_mastercard.get_stock_value(), 
-         )
+    def test_stock_worth(self):
+        stock_visa = Stock.objects.get(name='Visa')
+        stock_mastercard = Stock.objects.get(name='Mastercard')
+        self.assertEqual(
+            stock_visa.get_stock_value(), "Visa is worth 399306000000")
+        self.assertEqual(
+            stock_mastercard.get_stock_value(), 
+        )
          
