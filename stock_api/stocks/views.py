450a7cc8bae8788a481e4ec0ef8ec3a2338897b5
pythonredhat <pythonredhat1987@gmail.com>
2019-08-19 17:46:29 -0400

    Import module for views

--- stock_api/stocks/views.py
+++ stock_api/stocks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Stock
+from .serializers import StockSerializer
+
+
 # Create your views here.
