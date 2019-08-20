a194e63c926c0dc33dc34e76e7cc6f13c813b8bd
pythonredhat <pythonredhat1987@gmail.com>
2019-08-20 16:16:09 -0400

    Fix syntax error in regex on url for stock app

--- stock_api/stocks/urls.py
+++ stock_api/stocks/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(
-        r'^api/v1/stocks/(?P<pk>[0-9+)$',
+        r'^api/v1/stocks/(?P<pk>[0-9]+)$',
         views.get_delete_update_stock,
         name='get_delete_update_stock'
     ),
