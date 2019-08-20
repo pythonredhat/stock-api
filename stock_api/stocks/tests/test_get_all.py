b7b0022c2bfe0a38de609658a63c68a29e7296e2
pythonredhat <pythonredhat1987@gmail.com>
2019-08-20 16:34:03 -0400

    Fix syntax error

--- stock_api/stocks/tests/test_get_all.py
+++ stock_api/stocks/tests/test_get_all.py
@@ -13,13 +13,13 @@ class GetAllStocksTest(TestCase):
 
     def setUp(self):
         Stock.objects.create(
-            name="Amazon", ticker="AMZN", market_cap=891063000000 eps=24.10)
+            name="Amazon", ticker="AMZN", market_cap=891063000000, eps=24.10)
          Stock.objects.create(
-            name="Microsoft", ticker="MSFT", market_cap=1066000000000 eps=5.06)
+            name="Microsoft", ticker="MSFT", market_cap=1066000000000, eps=5.06)
          Stock.objects.create(
-            name="Apple", ticker="AAPL", market_cap=950000000000 eps=11.78)
+            name="Apple", ticker="AAPL", market_cap=950000000000, eps=11.78)
          Stock.objects.create(
-            name="Verizon", ticker="VZ", market_cap=232719000000 eps=3.83)
+            name="Verizon", ticker="VZ", market_cap=232719000000, eps=3.83)
         )
     
     def test_get_all_stocks(self):
@@ -32,3 +32,4 @@ class GetAllStocksTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         
+
